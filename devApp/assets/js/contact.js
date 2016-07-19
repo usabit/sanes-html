d9df1befeb1fd5c7c44f7185006ab2d561ad92d2
cassio mansini <cassiomansini@gmail.com>
2016-07-19 12:34:46 -0300

    alterações para mobile e ajustes para safari

--- devApp/assets/js/contact.js
+++ devApp/assets/js/contact.js
@@ -4,8 +4,10 @@
 
   $('nav.contact-type-list a').on('click', function(){
     $('nav.contact-type-list a').removeClass('active');
+    $('nav.contact-type-list a').removeClass('no-shadow');
     
     $(this).addClass('active');
+    $(this).prev('a').addClass('no-shadow');
 
     $('section.contact form').hide();
     $('section.contact form[name="' + $(this).data('form') + '"]').show();
